ff4a5b1364ffbc282987be93268b425555c0a24d
JHatch94 <35378910+JHatch94@users.noreply.github.com>
2019-10-16 14:56:41 -0600

    Cool

--- MegaDesk-Hatch/MainMenu.cs
+++ MegaDesk-Hatch/MainMenu.cs
@@ -12,11 +12,14 @@ namespace MegaDesk_Hatch
 {
     public partial class MainMenu : Form
     {
+        
+        //Main menu initialize
         public MainMenu()
         {
             InitializeComponent();
         }
 
+        //Add new quote button
         private void BtnAddNewQuote_Click(object sender, EventArgs e)
         {
             var addQuote = new AddQuote();
@@ -25,16 +28,25 @@ namespace MegaDesk_Hatch
             this.Hide();
         }
 
+        //View all quotes button
         private void BtnViewQuotes_Click(object sender, EventArgs e)
         {
-
+            var viewAllQuotes = new ViewAllQuotes();
+            viewAllQuotes.Tag = this;
+            viewAllQuotes.Show();
+            this.Hide();
         }
 
+        //Search quotes
         private void BtnSearchQuotes_Click(object sender, EventArgs e)
         {
-
+            var searchQuotes = new SearchQuotes();
+            searchQuotes.Tag = this;
+            searchQuotes.Show();
+            this.Hide();
         }
 
+        //Exit the program
         private void BtnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
