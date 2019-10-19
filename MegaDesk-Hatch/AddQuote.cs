e91adc86d109886417a3756c595935b986d61c58
Fritz Fevrier <fev17001@byui.edu>
2019-10-19 15:20:24 -0600

    add changes to files addquote and displayquote

--- MegaDesk-Hatch/AddQuote.cs
+++ MegaDesk-Hatch/AddQuote.cs
@@ -17,8 +17,30 @@ namespace MegaDesk_Hatch
         public AddQuote( )
         {   //Form mainMenu
             InitializeComponent();
-            //_mainMenu = mainMenu;
+            //_mainMenu = mainMenu
 
+        }
+
+        private void widthupanddown_Validating(object sender, CancelEventArgs e)
+        {
+            //size of desk in width(min 24"; max 96"
+            
+        }
+
+        private void depthupdown_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //size of desk in  depth (min 12"; max 48")
+           
+        }
+
+
+        private void AddQuote_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            var mainMenu = (Form)this.Tag;
+            mainMenu.Show();
+        }
+        private void AddQuote_Load(object sender, EventArgs e)
+        {
             //Populate SurfaceMaterials dropdown menu
             var materials = new List<Desk.DesktopMaterial>();
 
@@ -39,18 +61,46 @@ namespace MegaDesk_Hatch
 
             comShippingMethod.SelectedIndex = 0;
 
-
         }
 
-        private void AddQuote_FormClosed(object sender, FormClosedEventArgs e)
+
+        private void Get_NewQuote_Click(object sender, EventArgs e)
         {
-            var mainMenu = (Form)this.Tag;
-            mainMenu.Show();
-        }
-        private void AddQuote_Load(object sender, EventArgs e)
-        {
+            //This if statement is to check if the add quote form is complete.
 
+            // gets the information of the desk.  // needs to fill up this section
+            Desk NameOfNewDesk = new Desk();
+           
+
+            //gets the information entered by the user. // needs to fill up this section
+            DeskQuote NewDeskQuote = new DeskQuote();
+         
+
+            // get the quote amount and add amount to quote
+         
+            NewDeskQuote.Desk = NameOfNewDesk;
+            DateTime today = DateTime.Today;
+            NewDeskQuote.QuoteDate = today;
+            NewDeskQuote.QuotePrice = NewDeskQuote.GetQuotePrice();
+
+
+
+            try
+            {
+                // add quote to file
+                AddQuoteToFile(NewDeskQuote);
+
+                //show displayquote form
+                DisplayQuote NewDeskForm = new DisplayQuote(NewDeskQuote);
+                NewDeskForm.Show();
+                Hide();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("There was an error creating the quote. {0}", err.Message);
+            }
         }
+
 
 
         private void BtnCancel_Click(object sender, EventArgs e)
@@ -77,25 +127,42 @@ namespace MegaDesk_Hatch
             var DeskQuote = new DeskQuote();
 
         }
-        private void AddQuoteToFile(DeskQuote deskquote)
+        //gets the new quote and adds it to the new file
+        public void AddQuoteToFile(DeskQuote NewDeskQuote)
         {
-            var quotesFile = @"quotes.json";
-            List<DeskQuote> deskQuotes = new List<DeskQuote>();
+            string quotesFile = @"quotes.json"; //add quotes.json
 
-            //read existing quotes
             if (File.Exists(quotesFile))
             {
+                List<DeskQuote> currentQuotes = new List<DeskQuote>();
+
                 using (StreamReader reader = new StreamReader(quotesFile))
                 {
                     string quotes = reader.ReadToEnd();
-                    if (quotes.Length > 0)
-                    {
-                        deskQuote = JsonConvert.DeserializeObject<List<DeskQuote>>(quotes);
-                    }
+                    //not sure why this not working
+                    currentQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(quotes);
+
+                    currentQuotes.Add(NewDeskQuote);
                 }
+                SaveQuotes(currentQuotes);
             }
-            deskQuotes.Add(deskQuote);
-            SaveQuotes(deskQuotes);
+            else
+            {
+                List<DeskQuote> currentQuotes = new List<DeskQuote>();
+                currentQuotes.Add(NewDeskQuote);
+                SaveQuotes(currentQuotes);
+            }
         }
+        private void SaveQuotes(List<DeskQuote> currentQuotes)
+        {
+            var quotesFile = @"quotes.json";
+            // not sure why this is not working
+            var quotes = JsonConvert.SerializeObject(currentQuotes);
+
+            File.WriteAllText(quotesFile, quotes);
+
+        }
+
+
     }
 }
--- MegaDesk-Hatch/DisplayQuote.cs
+++ MegaDesk-Hatch/DisplayQuote.cs
@@ -12,9 +12,32 @@ namespace MegaDesk_Hatch
 {
     public partial class DisplayQuote : Form
     {
-        public DisplayQuote()
+
+        private DeskQuote _deskQuote;
+        public DisplayQuote(DeskQuote NewDeskQuote)
         {
             InitializeComponent();
+            // we neeed to match these variables from your yours.
+            _deskQuote = NewDeskQuote;
+          //  widthupanddown.Value = NewDeskQuote.Desk.Width;
+          //  depthupdown.Value = NewDeskQuote.Desk.Depth;
+           // DesktopMaterial.SelectedValue = NewDeskQuote.Desk.SurfaceMaterial;
+           // Num_Drawers.SelectedValue = NewDeskQuote.Desk.NumberOfDrawers;
+           // Delivery_Time.SelectedValue = NewDeskQuote.ShippingType;
+          //  CustomerName.Text = NewDeskQuote.CustomerName;
+           // TotalPrice.Text = NewDeskQuote.QuotePrice.ToString();
         }
+
+        private void DisplayQuote_Load(object sender, EventArgs e)
+        {
+            var mainMenu = new MainMenu();
+            mainMenu.Show();
+            Close();
+        }
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
     }
 }
